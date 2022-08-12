b67dc0592fd6e302d76cf92765215747bcf33258
DevTed-kr <104476000+devAccTedlee@users.noreply.github.com>
2022-08-12 15:35:26 +0900

    test component

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './home/home.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
