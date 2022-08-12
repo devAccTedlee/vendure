78ed4274757e97bfcbc308aed11bd3b0a6664b30
DevTed-kr <104476000+devAccTedlee@users.noreply.github.com>
2022-08-12 15:37:52 +0900

    rollback test data

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './home/home.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
